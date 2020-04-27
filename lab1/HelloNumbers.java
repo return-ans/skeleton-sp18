e07e7be2dbb5f92642b658e09cbf11256b8fb1d7
return-ans <1060782631@qq.com>
2020-04-27 03:41:35 -0700

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+/** Class that determines whether or not a year is a leap year.
+ *  @author YOUR NAME HERE
+ */
+public class HelloNumbers {
+
+    public static void main(String[] args) {
+        int x=0;
+        int curSum=0;
+        while(x<10){
+            System.out.print(curSum+" ");
+            curSum+=++x;
+        }
+    }
+}
+
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,10 @@
+/** Class that determines whether or not a year is a leap year.
+ *  @author YOUR NAME HERE
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
