f9d962789b9f18f5907af129bea3822c7b16684d
return-ans <1060782631@qq.com>
2020-04-27 03:54:57 -0700

    completed second part of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,17 @@
  */
 public class LeapYear {
 
+    /** isLeapYear 
+     *  divisible by 400 or
+     *  divisible by 4 and not by 100
+     */
+    public static boolean isLeapYear(int year){
+        if((year%400==0)||(year%4==0&&year%100!=0)){
+            return true;
+        }
+        return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
