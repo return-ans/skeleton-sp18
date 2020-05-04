b91fb79f1502be0370cb69307bd8e5d6a5e959a8
return-ans <1060782631@qq.com>
2020-05-04 04:18:04 -0700

    finish hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,143 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+    private int fillCount;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     * Some methods inherit, and some methods override
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public int capacity() {
+        return rb.length;
+    }
+
+    @Override
+    public int fillCount() {
+        return fillCount;
+    }
+
+    @Override
+    public void enqueue(T x) {
+        if (isFull() == true) {
+            throw new RuntimeException("Ring buffer overflow");
+        } else {
+            if (isEmpty() != true) {
+                last++;
+                last %= capacity(); //circle
+            }
+            rb[last] = x;
+            fillCount++;
+        }
+
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    public T dequeue() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            T ret = rb[first];
+            rb[first] = null;
+            if (fillCount() > 1) {
+                first++;
+                first %= capacity(); //circle
+            }
+            fillCount--;
+            return ret;
+        }
+
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            return rb[first];
+        }
+
+    }
+
+    // DONE: When you get to part 5, implement the needed code to support iteration.
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayRingBufferIterator();
+    }
+
+    private class ArrayRingBufferIterator implements Iterator<T> {
+        private int curPos;
+        private int count;
+
+        public ArrayRingBufferIterator() {
+            curPos = first;
+            count = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return count < fillCount();
+        }
+
+        @Override
+        public T next() {
+            T curItem = rb[curPos];
+            curPos++;
+            curPos %= capacity();
+            count++;
+            return curItem;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        //compare to itself
+        if(obj==this){
+            return true;
+        }
+        if(obj==null){
+            return false;
+        }
+        if(obj.getClass()!=this.getClass()){
+            return false;
+        }
+        ArrayRingBuffer<T> o=(ArrayRingBuffer<T>) obj;
+        if(o.fillCount()!=this.fillCount()){
+            return false;
+        }
+        Iterator<T> thisIt=this.iterator();
+        Iterator<T> objIt=o.iterator();
+        while(thisIt.hasNext()&&objIt.hasNext()){
+            if(thisIt.next()!=objIt.next()){
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,18 +1,48 @@
 package synthesizer;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+/**
+ * Tests the ArrayRingBuffer class.
+ *
+ * @author Josh Hug
  */
 
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<Integer>(10);
+        System.out.println(arb.isEmpty());
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        System.out.println(arb.dequeue());
+        System.out.println(arb.dequeue());
+        arb.enqueue(4);
+        System.out.println(arb.fillCount());
+        System.out.println(arb.dequeue());
+        System.out.println(arb.fillCount());
+        arb.enqueue(5);
+        arb.enqueue(6);
+        arb.enqueue(7);
+        arb.enqueue(8);
+        arb.enqueue(9);
+        System.out.println(arb.fillCount());
+        System.out.println(arb.dequeue());
+        System.out.println(arb.fillCount());
+        arb.enqueue(10);
+        arb.enqueue(11);
+        arb.enqueue(12);
+        arb.enqueue(13);
+
+
     }
 
-    /** Calls tests for ArrayRingBuffer. */
+    /**
+     * Calls tests for ArrayRingBuffer.
+     */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
