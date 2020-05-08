52165b23de06ab58e55c1ad1568f1229ac451205
return-ans <1060782631@qq.com>
2020-05-08 06:22:55 -0700

    finish lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -6,7 +6,7 @@ import java.util.Set;
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author LZ
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -27,6 +27,7 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     private Node root;  /* Root node of the tree. */
     private int size; /* The number of key-value pairs in the tree */
+    //put directly if size==0
 
     /* Creates an empty BSTMap. */
     public BSTMap() {
@@ -36,44 +37,112 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Removes all of the mappings from this map. */
     @Override
     public void clear() {
-        root = null;
+        root = null; //sentinel node
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
-     */
-    private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+    private void validate(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("null key");
+        }
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
+     */
+//    private V getHelper(K key, Node p) {
+//        throw new UnsupportedOperationException();
+//    }
+
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        validate(key);
+        if (size == 0) {
+            return null;
+        }
+        Node tmp = root;
+        while (tmp != null) {
+            int cmp = key.compareTo(tmp.key);
+            if (cmp == 0) {
+                return tmp.value;
+            }
+            if (cmp > 0) {
+                //this is bigger than tmp, move to right
+                tmp = tmp.right;
+            } else {
+                //less than tmp, move to left
+                tmp = tmp.left;
+            }
+        }
+        return null;
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
-    private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
-    }
+//    private Node putHelper(K key, V value, Node p) {
+//        throw new UnsupportedOperationException();
+//    }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY into a null child
+     * Special!! if size==1, put directly
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("Null key not allowed.");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("Null values not allowed.");
+        }
+        if (size == 0) {
+            //put directly if size==0
+            root = new Node(key, value);
+            size++;
+            return;
+        }
+        Node tmp = root;
+        int cmp;
+        while (true) {
+            //leaf node
+            cmp = key.compareTo(tmp.key);
+            if (cmp == 0) {
+                tmp.value = value;
+                return;
+            }
+            if (cmp > 0) {
+                //this is bigger than tmp, move to right if right is not null
+                if (tmp.right == null) {
+                    tmp.right = new Node(key, value); //insert if it's null
+                    size++;
+                    return;
+                }
+                tmp = tmp.right;
+            } else {
+                //less than tmp, move to left if left is not null
+                if (tmp.left == null) {
+                    tmp.left = new Node(key, value); //insert if it's null
+                    size++;
+                    return;
+                }
+                tmp = tmp.left;
+            }
+        }
+
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -84,18 +153,20 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
         throw new UnsupportedOperationException();
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -4,25 +4,27 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- *  A hash table-backed Map implementation. Provides amortized constant time
- *  access to elements via get(), remove(), and put() in the best case.
+ * A hash table-backed Map implementation. Provides amortized constant time
+ * access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ * @author LZ
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
     private static final int DEFAULT_SIZE = 16;
     private static final double MAX_LF = 0.75;
 
-    private ArrayMap<K, V>[] buckets;
-    private int size;
+    private ArrayMap<K, V>[] buckets; //2-D array
+    //same hashcode in same ArrayMap!!
+    private int size; //the number of key-value pairs
 
-    private int loadFactor() {
-        return size / buckets.length;
+    private double loadFactor() {
+        return (double) size / buckets.length;
     }
 
     public MyHashMap() {
         buckets = new ArrayMap[DEFAULT_SIZE];
+        //ArrayMap class in bucket are null initially!!!!!
         this.clear();
     }
 
@@ -35,9 +37,40 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
         }
     }
 
-    /** Computes the hash function of the given key. Consists of
-     *  computing the hashcode, followed by modding by the number of buckets.
-     *  To handle negative numbers properly, uses floorMod instead of %.
+    /**
+     * Recalculate hashcode to put in the new array
+     * Reorganise the bucket
+     *
+     * @param newBucketNum
+     */
+    private void resize(int newBucketNum) {
+        ArrayMap<K, V>[] temp = new ArrayMap[newBucketNum];
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] != null && buckets[i].size() > 0) {
+                Set<K> st = buckets[i].keySet();
+                for (K k : st) {
+                    V v = buckets[i].get(k);
+                    int newHashcode = Math.floorMod(k.hashCode(), newBucketNum);
+                    //new position in the new array
+                    //new a ArrayMap in the bucket if it's null first
+                    if (temp[newHashcode] == null) {
+                        //not null
+                        temp[newHashcode] = new ArrayMap<>();
+                    }
+                    //should put in an exist ArrayMap in the bucket
+                    temp[newHashcode].put(k, v);
+                }
+            }
+        }
+        buckets = temp;
+    }
+
+    /**
+     * Computes the hash function of the given key. Consists of
+     * computing the hashcode, followed by modding by the number of buckets.
+     * To handle negative numbers properly, uses floorMod instead of %.
+     * <p>
+     * Calculate the hashcode as the index of array to find item
      */
     private int hash(K key) {
         if (key == null) {
@@ -53,19 +86,54 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("Null key not allowed.");
+        }
+        if (size == 0) {
+            return null;
+        }
+        /*
+        this hashcode hashes to an ArrayMap class,
+        may has some key-value pairs
+         */
+        return buckets[hash(key)].get(key);
+
     }
 
-    /* Associates the specified value with the specified key in this map. */
+    /* Associates the specified value with the specified key in this map.
+     *  If the key is already exist, just change its value and increase the size.
+     * */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (key == null) {
+            throw new IllegalArgumentException("Null key not allowed.");
+        }
+        if (value == null) {
+            throw new IllegalArgumentException("Null values not allowed.");
+        }
+        if (get(key) != null) {
+            buckets[hash(key)].put(key, value);
+            //just change the value
+            return;
+        }
+        if (loadFactor() >= MAX_LF) {
+            //double the number of the bucket
+            resize(buckets.length * 2);
+        }
+        if (buckets[hash(key)] == null) {
+            //not null
+            buckets[hash(key)] = new ArrayMap<>();
+            //guarantee that not put a ket-value pair into a null ArrayMap
+        }
+        //should put in an exist ArrayMap in the bucket
+        buckets[hash(key)].put(key, value);
+        size++;
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
